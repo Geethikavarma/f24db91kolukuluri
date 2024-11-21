f6936c32e80b0ccb01a41fe90bb04c9ce53701aa
Geethika Kolukuluri <97678018+Geethikavarma@users.noreply.github.com>
2024-11-20 23:46:11 -0600

    made some changes in the project

--- routes/resource.js
+++ routes/resource.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const apiController = require('../controllers/api');
-const artifactController = require('../controllers/artifactsController');
+
+// Require controller modules
+const api_controller = require('../controllers/api');
+const heritageSiteController = require('../controllers/artifactsController');
 
 // Base API route
-router.get('/', apiController.api);
+router.get('/', api_controller.api);
 
-// Artifact-specific routes
-router.get('/artifacts', artifactController.artifact_list);
-router.post('/artifacts', artifactController.artifact_create_post);
-router.get('/artifacts/:id', artifactController.artifact_detail);
-router.put('/artifacts/:id', artifactController.artifact_update_put);
-router.delete('/artifacts/:id', artifactController.artifact_delete);
+// API routes for Heritage Site
+router.post('/artifacts', heritageSiteController.heritageSite_create_post);
+router.get('/artifacts', heritageSiteController.heritageSite_list);
+router.get('/artifacts/all', heritageSiteController.heritageSite_view_all_Page);
+/* GET create heritage site page */
+router.get('/artifacts/create', heritageSiteController.heritageSite_create_Page);
+
+// GET detail page for a specific Heritage Site (using query parameter)
+router.get('/artifacts/detail', heritageSiteController.heritageSite_view_one_Page);
+/* GET update page for heritage site */
+router.get('/update', heritageSiteController.heritageSite_update_Page);
+router.get('/artifacts/:id', heritageSiteController.heritageSite_detail);
+router.put('/artifacts/:id', heritageSiteController.heritageSite_update_put);
+router.delete('/artifacts/:id', heritageSiteController.heritageSite_delete);
 
 module.exports = router;
