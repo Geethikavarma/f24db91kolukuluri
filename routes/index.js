5831f1bc0600176577590e844cc0d44e7395fbde
Geethika Kolukuluri <97678018+Geethikavarma@users.noreply.github.com>
2024-11-21 16:46:09 -0600

    made some changes in the project

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET grid page with query parameters */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  let query = req.query;
-  console.log(`rows ${query.rows}`);
-  console.log(`cols ${query.cols}`);
-  res.render('grid', { title: 'Grid Display', query: query });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
