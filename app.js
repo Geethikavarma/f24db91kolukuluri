f3d7b454d4ffe38edd6da1403a2c92089d2d928a
Geethika Kolukuluri <97678018+Geethikavarma@users.noreply.github.com>
2024-11-21 21:21:03 -0600

    made some changes on my web page

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var resourceRouter = require('./routes/resource');  // Resource router
 
 // MongoDB imports
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://s574391:Geethika22@cluster0.ijbao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+
+mongoose.connect('mongodb+srv://ajithanarra24:ugekUdGXDkr3vqem@cluster0.m85be.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   serverSelectionTimeoutMS: 10000,
 }).then(() => {
   console.log('Connected to MongoDB Atlas');
@@ -24,26 +25,6 @@ mongoose.connect('mongodb+srv://s574391:Geethika22@cluster0.ijbao.mongodb.net/?r
 });
 const Artifact = require("./models/artifacts");
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    Account.findOne({ username: username })
-      .then(function (user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      })
-      .catch(function (err) {
-        return done(err)
-      })
-  })
-)
 var app = express();
 
 // MongoDB connection setup
@@ -53,18 +34,12 @@ app.use(logger('dev'));
 app.use(express.json());  // Add body parser middleware to handle JSON payload
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(express.static(path.join(__dirname, 'public')));
+
 // View engine setup (pug)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 // Routes setup
 app.use('/resource', resourceRouter);  // API for resource routes
 app.use('/grid', gridRouter);  // Route for /grid
@@ -72,20 +47,14 @@ app.use('/artifacts', artifactsRouter);  // Route for /artifacts
 app.use('/pick', pickRouter);  // Route for /pick
 app.use('/', indexRouter);  // Route for the homepage
 app.use('/users', usersRouter);  // Route for users
-// passport config
-// Use the existing connection
-// The Account model 
-var Account =require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser())
+
 // Error handler for 404
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // General error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
