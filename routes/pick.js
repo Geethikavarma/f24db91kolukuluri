20b4e3f8e35eeb259b1bd94882fe6f24b338a764
Geethika Kolukuluri <97678018+Geethikavarma@users.noreply.github.com>
2024-12-04 19:32:11 -0600

    Update pick.js

--- routes/pick.js
+++ routes/pick.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // Array of image file names
-  const image_names = [ 'Halloween.jpg', 'Diwali.jpg', 'snow.jpg', 'Winter.jpg', 'Fall.jpg'];
+  const image_names = [ 'item1.jpg', 'item2.jpg', 'item3.jpg', 'item4.jpg', 'item5.jpg'];
 
   res.render('randomitem', { title: 'A random item', image_names: image_names });
 });
